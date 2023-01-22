bee8dea4e28ff74e6a7d54d1bb710b09726b179f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-22 09:57:09 +0530

    added temp img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import logo from "./images/logo.png";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  );
 }
 
 export default App;
