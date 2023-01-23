29d735fa49d7485f24b3575600c3d34e91ce4401
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-23 20:34:39 +0530

    Changing FlexWrapper elements according to props.

--- src/UI/FlexWrapper/FlexWrapper.js
+++ src/UI/FlexWrapper/FlexWrapper.js
@@ -2,5 +2,21 @@ import "./FlexWrapper.css";
 import React from "react";
 
 export default function FlexWrapper(props) {
-  return <div className={`flex ${props.className}`}>{props.children}</div>;
+  return (
+    <>
+      {" "}
+      {props.element === "nav" && (
+        <nav className={`flex ${props.className}`}>{props.children}</nav>
+      )}
+      {props.element === "div" && (
+        <div className={`flex ${props.className}`}>{props.children}</div>
+      )}
+      {props.element === "ul" && (
+        <ul className={`flex ${props.className}`}>{props.children}</ul>
+      )}
+      {props.element === "li" && (
+        <li className={`flex ${props.className}`}>{props.children}</li>
+      )}
+    </>
+  );
 }
