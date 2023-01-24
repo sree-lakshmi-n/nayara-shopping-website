d8ae88c50191f20fe6216b7597ff7fddda920607
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-24 16:10:50 +0530

    categories section component

--- src/components/CategoriesSection/CategoriesSection.js
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -1,6 +1,19 @@
 import "./CategoriesSection.css";
 import React from "react";
+import Container from "../../UI/Container/Container";
 
-export default function CategoriesSection() {
-  return <section>CategoriesSection</section>;
+export default function CategoriesSection(props) {
+  return (
+    <section className="categories-section">
+      <Container className="catergories-wrapper">
+        {props.categories.map((category) => {
+          return (
+            <div className="category-card jewellery-img">
+              <button className="btn">jewellery</button>
+            </div>
+          );
+        })}
+      </Container>
+    </section>
+  );
 }
