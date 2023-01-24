8c413fd44f4168033662d7f0e987dd9f49811da1
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-24 12:42:33 +0530

    hero section component

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,5 +1,30 @@
 import React from "react";
+import heroImage from "../../images/heroimage.png";
+import "./HeroSection.css";
 
 export default function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <section className="hero-section">
+      <div className="hero">
+        <div className="hero-imagebox">
+          <img
+            src={heroImage}
+            alt="family with shopping bags jumping in joy"
+            className="hero-img"
+          />
+        </div>
+        <div className="hero-textbox">
+          <h1 className="heading-primary">
+            Kick of the new year with the best of the categories
+          </h1>
+          <p className="hero-description">
+            Electronics, Jewellery, Clothing, and more
+          </p>
+          <a href="#home" className="btn btn-cta">
+            Shop now &rarr;
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
