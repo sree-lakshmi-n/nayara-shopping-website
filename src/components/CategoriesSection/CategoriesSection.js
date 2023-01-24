e834a0c9c83a72068ac3d0d7983355125a0b287d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-24 16:42:00 +0530

    moved category card component to CategoryCard.js

--- src/components/CategoriesSection/CategoriesSection.js
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -1,17 +1,14 @@
 import "./CategoriesSection.css";
 import React from "react";
 import Container from "../../UI/Container/Container";
+import CategoryCard from "../CategoryCard/CategoryCard";
 
 export default function CategoriesSection(props) {
   return (
     <section className="categories-section">
       <Container className="catergories-wrapper">
         {props.categories.map((category) => {
-          return (
-            <div className="category-card jewellery-img">
-              <button className="btn">jewellery</button>
-            </div>
-          );
+          return <CategoryCard category={category} />;
         })}
       </Container>
     </section>
