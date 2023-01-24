ce3e21629e563a6656f984032a6458d55b4f8576
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-24 17:34:21 +0530

    added btn-category class to the category btn

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -11,7 +11,7 @@ export default function CategoryCard(props) {
         .join("")}-img`}
       element="div"
     >
-      <button className="btn">{props.category}</button>
+      <button className="btn btn-category">{props.category}</button>
     </FlexWrapper>
   );
 }
