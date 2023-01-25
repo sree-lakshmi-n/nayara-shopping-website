869f5104684a9074f8d9dc06886eeb9aafe86267
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-25 11:41:28 +0530

    CategoryCard components added

--- src/components/CategoriesSection/CategoriesSection.js
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -7,8 +7,8 @@ export default function CategoriesSection(props) {
   return (
     <section className="categories-section">
       <GridWrapper className="categories-wrapper grid--2-cols">
-        {props.categories.map((category) => {
-          return <CategoryCard category={category} />;
+        {props.categories.map((category, index) => {
+          return <CategoryCard category={category} key={index} />;
         })}
       </GridWrapper>
     </section>
