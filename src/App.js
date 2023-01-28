f63d4b942440c02cbddc810ba3ae58d8ac52569d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-28 16:27:33 +0530

    Routes to home and categories declared

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Products from "./components/Products/Products";
 
 function App() {
   const categories = [
@@ -10,7 +13,32 @@ function App() {
   ];
   return (
     <main>
-      <Home categories={categories} />
+      <BrowserRouter>
+        <NavBar categories={categories} />
+        <Routes>
+          <Route exact path="/" element={<Home categories={categories} />} />
+          <Route
+            exact
+            path="/jewellery"
+            element={<Products category="jewelery" />}
+          />
+          <Route
+            exact
+            path="/electronics"
+            element={<Products category="electronics" />}
+          />
+          <Route
+            exact
+            path="/men's clothing"
+            element={<Products category="men's clothing" />}
+          />
+          <Route
+            exact
+            path="/women's clothing"
+            element={<Products category="women's clothing" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
