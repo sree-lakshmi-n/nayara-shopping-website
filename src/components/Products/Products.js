8487a6cfaf21f88ca990df49ddfb3e8f9d7d0472
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-29 18:02:30 +0530

    loading image added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,9 +4,10 @@ import GridWrapper from "../../UI/GridWrapper/GridWrapper";
 import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 import ProductImageBox from "../ProductImageBox/ProductImageBox";
 import { Link } from "react-router-dom";
+import loading from "../../images/loading.gif";
 
 export default function Products(props) {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState("");
   console.log(props.category);
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/category/${props.category}`)
@@ -14,26 +15,33 @@ export default function Products(props) {
       .then((json) => setProducts(json));
   }, [props.category]);
   return (
-    <GridWrapper className="products-container grid--2-cols">
-      {products.map((product) => {
-        return (
-          <FlexWrapper
-            key={product.id}
-            className="pdt-wrapper flex-dirn-col"
-            element="div"
-          >
-            <ProductImageBox image={product.image} />
+    <>
+      {!products && (
+        <div className="loading">
+          <img src={loading} alt="loading animation" />
+        </div>
+      )}
+      {products && (
+        <GridWrapper className="products-container grid--2-cols">
+          {products.map((product) => {
+            return (
+              <FlexWrapper
+                key={product.id}
+                className="pdt-wrapper flex-dirn-col"
+                element="div"
+              >
+                <ProductImageBox image={product.image} />
 
-            <div className="pdt-info">
-              <span className="pdt-cost">${product.price}</span>
-              <a href="#id">
-                <ion-icon name="cart-outline"></ion-icon>
-              </a>
-            </div>
-            <Link to={`/${props.category}/${product.id}`}>More info</Link>
-          </FlexWrapper>
-        );
-      })}
-    </GridWrapper>
+                <div className="pdt-info">
+                  <span className="pdt-rating">{product.rating.rate}</span>
+                  <span className="pdt-cost">${product.price}</span>
+                </div>
+                <Link to={`/${props.category}/${product.id}`}>More info</Link>
+              </FlexWrapper>
+            );
+          })}
+        </GridWrapper>
+      )}
+    </>
   );
 }
