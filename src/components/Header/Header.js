e8996928e0d5ba005181da23f7b5c8771d89770e
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-30 13:03:30 +0530

    Renamed to Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
+import Logo from "../Logo/Logo";
+import NavCategories from "../NavCategories/NavCategories";
+import NavOptions from "../NavOptions/NavOptions";
+import "./Header.css";
+
+export default function NavBar(props) {
+  return (
+    <header className="main-nav">
+      <FlexWrapper className="flex-center-align nav__top" element="nav">
+        <Logo />
+        <NavCategories categories={props.categories} />
+        <NavOptions />
+      </FlexWrapper>
+    </header>
+  );
+}
