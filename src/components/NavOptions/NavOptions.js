93949624cc8f5af41e3e3bfcd58a47299d14de6d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-31 13:48:57 +0530

    handling authentication

--- src/components/NavOptions/NavOptions.js
+++ src/components/NavOptions/NavOptions.js
@@ -1,43 +1,47 @@
 import "./NavOptions.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../../StateProvider";
+import { auth } from "../../firebase";
 
-export default function NavOptions() {
+export default function NavOptions(props) {
   const [{ basket, user }, dispatch] = useStateValue();
-  const options = [
-    {
-      option: "login",
-      icon: "person",
-    },
-    {
-      option: "wishlist",
-      icon: "heart",
-    },
-    {
-      option: "cart",
-      icon: "cart",
-    },
-  ];
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut().then(() => {
+        console.log("Sign-out successful.");
+      });
+    }
+  };
+
   return (
     <nav className="nav__options">
       <FlexWrapper
         className="nav__options-wrapper flex-center-align"
         element="ul"
       >
-        {options.map((option, index) => (
-          <FlexWrapper key={index} className="nav-option-item" element="ul">
-            <Link to={`/${option.option}`}>
-              {" "}
-              <ion-icon name={`${option.icon}-outline`}></ion-icon>
-            </Link>
-            {option.option === "cart" && (
-              <span className="cart-total">{basket?.length}</span>
-            )}
-            <span className="option-label">{option.option}</span>
-          </FlexWrapper>
-        ))}
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">
+              Hello {!user ? "Guest" : user}
+            </span>
+            <br />
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
+
+        <FlexWrapper className="nav-option-item" element="ul">
+          <Link to="/checkout">
+            {" "}
+            <ion-icon name={`cart-outline`}></ion-icon>
+          </Link>
+          <span className="cart-total">{basket?.length}</span>
+          <span className="option-label">Cart</span>
+        </FlexWrapper>
       </FlexWrapper>
     </nav>
   );
