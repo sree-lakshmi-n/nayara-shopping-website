30a1c19f0602d2ab949a62efad27af78bc674895
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-01 17:50:33 +0530

    back arrow added. sign in -sign up functionality separated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,18 +9,19 @@ import {
 import logo from "../../images/logo.png";
 import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 import { useStateValue } from "../../StateProvider";
+import Container from "../../UI/Container/Container";
 
 export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const [{ basket, user }, dispatch] = useStateValue();
 
   const signIn = (e) => {
     e.preventDefault();
-
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
@@ -42,7 +43,6 @@ export default function Login() {
   };
   const signUp = (e) => {
     e.preventDefault();
-
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
@@ -62,41 +62,84 @@ export default function Login() {
         // ..
       });
   };
+  const signInHandler = () => {
+    setErrorMsg("");
+    setIsSignUp(false);
+  };
+  const signUpHandler = () => {
+    setErrorMsg("");
+    setIsSignUp(true);
+  };
 
   return (
-    <FlexWrapper className="login flex-dirn-col" element="div">
-      <Link to="/">
-        <img src={logo} alt="Nayara logo" className="login-logo" />
+    <Container>
+      <Link to="/" className="btn-back-arrow">
+        <ion-icon name="arrow-back-outline" className="back-arrow"></ion-icon>
       </Link>
-      <div className="login-container">
-        <h2 className="login-title">Sign-in</h2>
-        <span className="error">{errorMsg}</span>
-        <form className="login-form">
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email Address"
-          />
+      <FlexWrapper className="login flex-dirn-col" element="div">
+        <Link to="/">
+          <img src={logo} alt="Nayara logo" className="login-logo" />
+        </Link>
+        <div className="login-container">
+          <h2 className="login-title">{isSignUp ? "Sign Up" : "Sign In"}</h2>
+          <span className="error">{errorMsg}</span>
+          <form className="login-form">
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setErrorMsg("");
+              }}
+              placeholder="Email Address"
+            />
 
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-          />
-          <button
-            className="btn btn-login-submit"
-            type="submit"
-            onClick={signIn}
-          >
-            Sign In
-          </button>
-          <button className="btn" onClick={signUp}>
-            New to Nayara? Sign Up
-          </button>
-        </form>
-      </div>
-    </FlexWrapper>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setErrorMsg("");
+              }}
+              placeholder="Password"
+            />
+            {!isSignUp && (
+              <>
+                <button
+                  className="btn btn-login-submit"
+                  type="submit"
+                  onClick={signIn}
+                >
+                  Sign In
+                </button>
+                <p>
+                  New to Nayara?{" "}
+                  <span onClick={signUpHandler} className="login-link">
+                    Sign Up
+                  </span>
+                </p>
+              </>
+            )}
+            {isSignUp && (
+              <>
+                <button
+                  className="btn btn-login-submit"
+                  type="submit"
+                  onClick={signUp}
+                >
+                  Sign Up
+                </button>
+                <p>
+                  Already have an account?{" "}
+                  <span className="login-link" onClick={signInHandler}>
+                    Sign In
+                  </span>
+                </p>
+              </>
+            )}
+          </form>
+        </div>
+      </FlexWrapper>
+    </Container>
   );
 }
