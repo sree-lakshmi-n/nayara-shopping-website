999a311e7fea8b871956356a377dace27f37f520
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-01 10:40:53 +0530

    CheckoutProduct component

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,6 +1,30 @@
 import "./CheckoutProduct.css";
 import React from "react";
+import { useStateValue } from "../../StateProvider";
+import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 
-export default function CheckoutProduct() {
-  return <div>CheckoutProduct</div>;
+export default function CheckoutProduct(props) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    // remove the item from the basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: props.id,
+    });
+  };
+
+  return (
+    <div className="checkout-pdt">
+      <div className="checkout-pdt-image">
+        <img src={props.image} />
+      </div>
+      <FlexWrapper className="checkout-pdt-info flex-dirn-col" element="div">
+        <h2 className="checkout-pdt-title">{props.title}</h2>
+        <p className="checkout-pdt-price">${props.price}</p>
+        <p className="checkout-pdt-rating">{props.rating.rate}</p>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
+      </FlexWrapper>
+    </div>
+  );
 }
