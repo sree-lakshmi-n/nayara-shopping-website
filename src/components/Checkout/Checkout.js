790752f25b9dd75553d67d7044eb41fe638f92e6
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-01 10:40:36 +0530

    Checkout component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,6 +1,31 @@
 import "./Checkout.css";
 import React from "react";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
+import Subtotal from "../Subtotal/Subtotal";
+import { useStateValue } from "../../StateProvider";
+import Container from "../../UI/Container/Container";
+import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 
 export default function Checkout() {
-  return <div>Checkout</div>;
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <Container className="checkout">
+      <FlexWrapper className="checkout-left flex-dirn-col" element="div">
+        <h2>Hello, {user?.email}</h2>
+        <h3 className="checkout__title">Your shopping Basket</h3>
+        {basket.map((item) => (
+          <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
+      </FlexWrapper>
+      <div className="checkout-right">
+        <Subtotal />
+      </div>
+    </Container>
+  );
 }
